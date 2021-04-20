8e90effa8fe3570e27087bbe70f304c435e5be53
Jaahhh <james.ahfat@gmail.com>
2021-04-20 12:49:18 -0600

    add db connection and fileWrite functions

---
+++ index.js
@@ -0,0 +1,100 @@
+require('dotenv').config()
+const { basename } = require('path')
+const { unlink, access } = require('fs/promises')
+const { watch } = require('chokidar')
+const { constants, readFileSync, writeFile, mkdir } = require('fs')
+const sql = require('mssql')
+
+const log = console.log.bind(console)
+
+checkFor(process.env.WATCH_DIRECTORY)
+checkFor(process.env.OUTPUT_DIRECTORY)
+
+var watcher = watch(process.env.WATCH_DIRECTORY, {
+    awaitWriteFinish: {
+        stabilityThreshold: 1500,
+        pollInterval: 100,
+    },
+    ignored: /^(?=.*(\.\w+)$)(?!.*(\.txt)$).*$/,
+})
+
+watcher.on('add', (path) => fileOps(path))
+
+function fileOps(path) {
+    var array = readFileSync(path).toString().split(/\r?\n/)
+    var ext = basename(path)
+    connectDB(array, ext)
+    removeFile(path)
+}
+
+function connectDB(array, ext) {
+    var config = buildConfig(array)
+    sql.connect(config)
+        .then((pool) => {
+            return pool
+                .request()
+                .input('input_1', sql.Char, array[3])
+                .input('input_2', sql.Char, array[4])
+                .query(
+                    `SELECT RTRIM(CUENTA) AS CUENTA, RTRIM(CLIENTEID) AS CLIENTEID, RTRIM(DESCRIPCION) AS DESCRIPCION,
+          RTRIM(NOMBRE) AS NOMBRE, RTRIM(APELLIDO) AS APPELIDO, VALOR1, VALOR2, VALOR3, VALOR4, VALOR5, SOCIAL
+          FROM HRCU.dbo.SALDOS WHERE CUENTA=@input_1 AND CLIENTEID=@input_2`
+                )
+        })
+        .then((result) => {
+            writeToFile(JSON.stringify(result.recordsets[0]), ext)
+        })
+        .catch((err) => {
+            //do nothing with error for now
+        })
+}
+
+function buildConfig(array) {
+    var config = {
+        user: array[1],
+        password: array[2],
+        server: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        pool: {
+            max: 10,
+            min: 0,
+            idleTimeoutMillis: 750,
+        },
+    }
+
+    return config
+}
+
+function writeToFile(content, ext) {
+    writeFile(`${process.env.OUTPUT_DIRECTORY}/${ext}`, content, (err) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        log(`File ${ext} created in base directory`)
+    })
+}
+
+async function checkFor(path) {
+    try {
+        await access(path, constants.R_OK | constants.W_OK)
+        log('can access')
+    } catch (err) {
+        createDirectory(path)
+    }
+}
+
+async function createDirectory(path) {
+    mkdir(path, { recursive: true }, function (err) {
+        if (err) log(err)
+        else log(`Directory created at ${path}`)
+    })
+}
+
+async function removeFile(path) {
+    try {
+        await unlink(path)
+    } catch (error) {
+        log(error.message)
+    }
+}
